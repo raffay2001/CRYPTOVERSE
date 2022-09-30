e2fa80d9fd8ddaf2bc6d3c0f1aa4d1b6d93043b6
raffay2001 <raffay.ahmed009@gmail.com>
2022-09-30 17:45:24 +0500

    Worked on Chart Component.

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -14,8 +14,11 @@ import {
   NumberOutlined,
   ThunderboltOutlined,
 } from "@ant-design/icons";
-import { useGetCryptoDetailsQuery } from "../services/cryptoApi";
-
+import {
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} from "../services/cryptoApi";
+import LineChart from "./LineChart";
 const { Title, Text } = Typography;
 const { Option } = Select;
 
@@ -23,7 +26,11 @@ const CryptoDetails = () => {
   const { coinId } = useParams();
   const [timePeriod, setTimePeriod] = useState("7d");
   const { data, error, isLoading } = useGetCryptoDetailsQuery(coinId);
-  // console.log(data);
+  const { data: coinHistory, isLoading: coinHistoryIsLoading } =
+    useGetCryptoHistoryQuery({
+      coinId,
+      timePeriod,
+    });
   const cryptoDetails = data?.data?.coin;
   console.log(cryptoDetails);
 
@@ -112,7 +119,13 @@ const CryptoDetails = () => {
           <Option key={date}>{date}</Option>
         ))}
       </Select>
-      {/* line chart  */}
+
+      <LineChart
+        coinHistory={coinHistory}
+        currentPrice={millify(cryptoDetails.price)}
+        coinName={cryptoDetails.name}
+      />
+
       <Col className="stats-container">
         <Col className="coin-value-statistics">
           <Col className="coin-value-statistics-heading">
@@ -152,25 +165,25 @@ const CryptoDetails = () => {
       </Col>
       <Col className="coin-desc-link">
         <Row className="coin-desc">
-            <Title level={3} className="coin-details-heading">
-              What is {cryptoDetails.name}
-              {HTMLReactParser(cryptoDetails.description)}
-            </Title>
+          <Title level={3} className="coin-details-heading">
+            What is {cryptoDetails.name}
+            {HTMLReactParser(cryptoDetails.description)}
+          </Title>
         </Row>
         <Col className="coin-links">
-            <Title level={3} className="coin-details-heading">
-                {cryptoDetails.name} Links
-            </Title>
-            {cryptoDetails.links.map((link) => (
-              <Row className="coin-link" key={link.name}>
-                  <Title level={5} className="link-name">
-                    {link.type}
-                  </Title>
-                  <a href={link.url} target="_blank" rel="noreferrer">
-                    {link.name}
-                  </a>
-              </Row>
-            ))}
+          <Title level={3} className="coin-details-heading">
+            {cryptoDetails.name} Links
+          </Title>
+          {cryptoDetails.links.map((link) => (
+            <Row className="coin-link" key={link.name}>
+              <Title level={5} className="link-name">
+                {link.type}
+              </Title>
+              <a href={link.url} target="_blank" rel="noreferrer">
+                {link.name}
+              </a>
+            </Row>
+          ))}
         </Col>
       </Col>
     </Col>
---
+++ src/components/LineChart.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import { Col, Row, Typography } from "antd";
+
+const { Title } = Typography;
+
+const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  return (
+    <>
+        <Row className="chart-header">
+            <Title level={2} className="chart-title">{coinName} Price Chart</Title>
+            <Col className="price-container">
+
+            </Col>
+        </Row>
+    </>
+  );
+};
+
+export default LineChart;
