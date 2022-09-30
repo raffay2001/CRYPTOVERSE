27a381d96902a21bdfbf49583d278021f59bf565
raffay2001 <raffay.ahmed009@gmail.com>
2022-09-30 12:35:24 +0500

    Worked on News Component.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,16 +7,45 @@ const { Text, Title } = Typography;
 const { Option } = Select;
 
 const News = ({ simplified }) => {
+  const demoImg =
+    "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
   const {
     data: cryptoNews,
     error,
     isLoading,
   } = useGetCryptoNewsQuery({
     newsCategory: "Cryptocurrency",
-    count: simplified ? 10 : 100,
+    count: simplified ? 6 : 12,
   });
-  console.log(cryptoNews);
-  return <div>News</div>;
+
+  if (!cryptoNews?.value) return "Loading...";
+
+  return (
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, index) => (
+        <Col xs={24} sm={12} lg={8} key={index}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+                <img
+                  src={news?.image?.thumbnail?.contentUrl || demoImg}
+                  alt="news"
+                />
+              </div>
+              <p>
+                {news.description.length > 100
+                  ? `${news.description.substring(0, 100)}....`
+                  : news.description}
+              </p>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default News;
